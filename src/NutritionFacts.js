995776b50767e865e006dd2ecf0c547b1399cd81
Reese Marshall <114102254+Reese755@users.noreply.github.com>
2023-01-26 19:16:18 -0500

    First Commit

---
+++ src/NutritionFacts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import candy from './Candy'
+
+function NutritionFacts ({name, image, nutrition_facts}) {
+
+
+
+
+return (
+<div>
+<h2> Nutrition Facts</h2>
+<p>Serving Size: {nutrition_facts.serving_size} </p>
+<p></p>
+
+</div>
+    )
+}
+export default NutritionFacts;
